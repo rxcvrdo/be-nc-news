54f3f68ef8661bdd8e23ec6bd6993ef045888c2e
rxcvrdo <rmurdock177@hotmail.co.uk>
2024-07-15 15:00:35 +0100

    have access to api json object with testing completed too

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ const express = require('express')
 const app = express()
 const{getTopics} = require('./controllers/news-controller')
 const {psqlErrorHandler, customErrorHandler, serverErrorHandler} = require('./app-error-handlers')
+const endpoints =require('./endpoints.json')
 
 app.use(express.json())
 
 app.get('/api/topics',getTopics);
+
+app.get('/api',(req, res, next) => {
+    res.status(200).send({endpoints})
+})
 
 app.all('*', (req, res) => {
     res.status(404).send({ message: 'Not found' });
