09fe78ec6babc1cde02c4fafba8485b12f0cd800
rxcvrdo <rmurdock177@hotmail.co.uk>
2024-07-18 12:18:39 +0100

    start of next task

--- models/news-model.js
+++ models/news-model.js
@@ -8,7 +8,7 @@ exports.selectTopics = () => {
 }
 
 exports.fetchArticleById =(article_id) => {
-    return db.query('SELECT * FROM articles WHERE article_id = $1;', [article_id])
+    return db.query('SELECT articles.*, COUNT(comments.comment_id) AS comment_countFROM articles LEFT JOIN comments ON comments.article_id = articles.article_id WHERE articles.article_id = $1 GROUP BY articles.article_id;', [article_id])
     .then((result) => {
         if(result.rows.length ===0){
             return Promise.reject({status: 404, message: 'article does not exist'})
